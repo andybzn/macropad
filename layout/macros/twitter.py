8e11de2e14c9f9159381b396595de2ad9897f4c3
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2022-07-28 21:52:08 +0100

    inital macro for twitter

---
+++ layout/macros/twitter.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2022 Andy Baizon
+# SPDX-License-Identifier: MIT
+# Hotkeys for Twwitter
+
+app = {
+    "name": "lets shitpost",  # application
+    "macros": [  # macro list
+        # row one
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        # row two
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "?", [""]),  # nothing
+        # row three
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        # row four
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        # click pot
+        (0x000000, "", [""]),  # nothing
+    ],
+}

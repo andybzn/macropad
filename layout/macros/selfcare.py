52420d649300c273a5d4a593175a4900e3c21679
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2022-07-28 21:49:33 +0100

    selfcare macro

---
+++ layout/macros/selfcare.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2022 Andy Baizon
+# SPDX-License-Identifier: MIT
+# Hotkeys for Selfcare
+
+app = {
+    "name": "s e l f c a r e",  # application
+    "macros": [  # macro list
+        # row one
+        (0x000000, "how", [""]),  # nothing
+        (0x000000, "about", [""]),  # nothing
+        (0x000000, "we", [""]),  # nothing
+        # row two
+        (0x000000, "take", [""]),  # nothing
+        (0x000000, "a", [""]),  # nothing
+        (0x000000, "break?", [""]),  # nothing
+        # row three
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        # row four
+        (0x000000, "get", [""]),  # nothing
+        (0x000000, "some", [""]),  # nothing
+        (0x000000, "water", [""]),  # nothing
+        # click pot
+        (0x000000, "", [""]),  # nothing
+    ],
+}

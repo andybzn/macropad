a959e0d2991f9d2cdb62793c22f8fbdd16514452
dark-coffee <9117837+dark-coffee@users.noreply.github.com>
2022-07-30 18:57:08 +0100

    updated teams macro layout

--- layout/macros/microsoft-teams.py
+++ layout/macros/microsoft-teams.py
@@ -8,21 +8,21 @@ app = {
     "name": "MSFT Teams",  # application
     "macros": [  # macro list
         # row one
-        (0x004000, "Activity", [Keycode.ALT, "ONE"]),  # open activity pane
-        (0x004000, "Chat", [Keycode.ALT, "TWO"]),  # open chat pane
-        (0x400000, "Cmd", [Keycode.ALT, "K"]),  # open command bar
+        (0x004000, "Activity", [Keycode.CONTROL, "ONE"]),  # open activity pane
+        (0x004000, "Chat", [Keycode.CONTROL, "TWO"]),  # open chat pane
+        (0x400000, "Cmd", [Keycode.CONTROL, "/"]),  # open command bar
         # row two
-        (0x202000, "Teams", [Keycode.ALT, "THREE"]),  # open teams list pane
-        (0x202000, "Cal", [Keycode.ALT, "FOUR"]),  # open calendar pane
-        (0x400000, "Files", [Keycode.ALT, "FIVE"]),  # open files pane
+        (0x202000, "Teams", [Keycode.CONTROL, "THREE"]),  # open teams list pane
+        (0x202000, "Cal", [Keycode.CONTROL, "FOUR"]),  # open calendar pane
+        (0x400000, "Files", [Keycode.CONTROL, "FIVE"]),  # open files pane
         # row three
-        (0x000040, "", [Keycode.CONTROL, ""]),
-        (0x000040, "Camera", [Keycode.CONTROL, Keycode.ALT, "o"]),  # toggle camera
-        (0x000040, "", [Keycode.CONTROL, ""]),
+        (0x000040, "Join", [Keycode.CONTROL, Keycode.SHIFT, "J"]),  # join meeting
+        (0x000040, "Cam", [Keycode.CONTROL, Keycode.SHIFT, "O"]),  # toggle camera
+        (0x000040, "PTT", [Keycode.CONTROL, Keycode.SPACE]), # push to talk
         # row four
-        (0x000000, "Mute", [Keycode.CONTROL, Keycode.ALT, "n"]),  # toggle mute
-        (0x800000, "Accept", [Keycode.CONTROL, Keycode.ALT, "a"]),  # accept call
-        (0x101010, "Decline", [Keycode.CONTROL, Keycode.ALT, "d"]),  # decline call
+        (0x004000, "Acpt", [Keycode.CONTROL, Keycode.SHIFT, "S"]),  # accept call
+        (0x000000, "Mute", [Keycode.CONTROL, Keycode.SHIFT, "M"]),  # toggle mute
+        (0x800000, "Dclne", [Keycode.CONTROL, Keycode.SHIFT, "D"]),  # decline call
         # click pot
         (0x000000, "", [Keycode.ALT, "F4"]),  # nope out
     ],

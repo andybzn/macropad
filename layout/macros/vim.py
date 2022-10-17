ebb59f23b6c5f7680274cc9bcd6ac18e57443e80
Andy Baizon <dev@baizon.uk>
2022-10-17 20:53:01 +0100

    new layout
    vim.py - vim shortcuts

---
+++ layout/macros/vim.py
@@ -0,0 +1,30 @@
+
+# SPDX-FileCopyrightText: 2022 Andy Baizon
+# SPDX-License-Identifier: MIT
+# Hotkeys for Vim
+
+from adafruit_hid.keycode import Keycode  # import keycodes
+
+app = {
+    "name": "Vim",  # application
+    "macros": [  # macro list
+        # row one
+        (0x000040, ":q!", [":", "q", "!"]),  # hard quit 
+        (0x000010, "", [":", "w", "q"]),  # save and quit 
+        (0x000000, "", [""]),  # nothing
+        # row two
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        # row three
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        # row four
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        (0x000000, "", [""]),  # nothing
+        # click pot
+        (0x000000, "", [Keycode.ALT, "F4"]),  # nope out
+    ],
+}

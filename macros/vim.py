52a79982166e8dbfb4278bb4c1927c2362f13f07
Andy Baizon <dev@baizon.uk>
2022-10-21 08:36:32 +0100

    Update vim.py

--- macros/vim.py
+++ macros/vim.py
@@ -1,4 +1,3 @@
-
 # SPDX-FileCopyrightText: 2022 Andy Baizon
 # SPDX-License-Identifier: MIT
 # Hotkeys for Vim
@@ -10,8 +9,8 @@ app = {
     "macros": [  # macro list
         # row one
         (0x000040, ":q!", [":", "q", "!"]),  # hard quit 
-        (0x000010, "", [":", "w", "q"]),  # save and quit 
-        (0x000000, "", [""]),  # nothing
+        (0x000010, ":wq!", [":", "w", "q"]),  # save and quit 
+        (0x000000, "dd", ["d", "d"]),  # delete line
         # row two
         (0x000000, "", [""]),  # nothing
         (0x000000, "", [""]),  # nothing
